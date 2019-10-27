3603fe9d61d94d59bb7ffc11d3656a517a634feb
Dmitry Nikolaev <dnikolaev.job@gmail.com>
2019-10-27 18:43:11 +0100

    Project uploaded
    Fully functional and ready to visualize

---
+++ Fractals/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.render = new System.Windows.Forms.Button();
+            this.ReBox = new System.Windows.Forms.TextBox();
+            this.ImBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mandelSwitch = new System.Windows.Forms.RadioButton();
+            this.juliaSwitch = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zBox = new System.Windows.Forms.TextBox();
+            this.randomBtn = new System.Windows.Forms.Button();
+            this.defaultBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(729, 479);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // render
+            // 
+            this.render.Location = new System.Drawing.Point(375, 12);
+            this.render.Name = "render";
+            this.render.Size = new System.Drawing.Size(75, 49);
+            this.render.TabIndex = 1;
+            this.render.Text = "Render";
+            this.render.UseVisualStyleBackColor = true;
+            this.render.Click += new System.EventHandler(this.render_Click);
+            // 
+            // ReBox
+            // 
+            this.ReBox.Location = new System.Drawing.Point(551, 22);
+            this.ReBox.Name = "ReBox";
+            this.ReBox.ReadOnly = true;
+            this.ReBox.Size = new System.Drawing.Size(69, 22);
+            this.ReBox.TabIndex = 2;
+            // 
+            // ImBox
+            // 
+            this.ImBox.Location = new System.Drawing.Point(655, 22);
+            this.ImBox.Name = "ImBox";
+            this.ImBox.ReadOnly = true;
+            this.ImBox.Size = new System.Drawing.Size(69, 22);
+            this.ImBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(520, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "C=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(626, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "+ i";
+            // 
+            // nBox
+            // 
+            this.nBox.Location = new System.Drawing.Point(286, 25);
+            this.nBox.Name = "nBox";
+            this.nBox.Size = new System.Drawing.Size(69, 22);
+            this.nBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "N=";
+            // 
+            // mandelSwitch
+            // 
+            this.mandelSwitch.AutoSize = true;
+            this.mandelSwitch.Checked = true;
+            this.mandelSwitch.Location = new System.Drawing.Point(12, 12);
+            this.mandelSwitch.Name = "mandelSwitch";
+            this.mandelSwitch.Size = new System.Drawing.Size(100, 21);
+            this.mandelSwitch.TabIndex = 8;
+            this.mandelSwitch.TabStop = true;
+            this.mandelSwitch.Text = "Mandelbrot";
+            this.mandelSwitch.UseVisualStyleBackColor = true;
+            this.mandelSwitch.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mandelSwitch_MouseDown);
+            // 
+            // juliaSwitch
+            // 
+            this.juliaSwitch.AutoSize = true;
+            this.juliaSwitch.Location = new System.Drawing.Point(12, 40);
+            this.juliaSwitch.Name = "juliaSwitch";
+            this.juliaSwitch.Size = new System.Drawing.Size(58, 21);
+            this.juliaSwitch.TabIndex = 9;
+            this.juliaSwitch.Text = "Julia";
+            this.juliaSwitch.UseVisualStyleBackColor = true;
+            this.juliaSwitch.MouseDown += new System.Windows.Forms.MouseEventHandler(this.juliaSwitch_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(147, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Z^";
+            // 
+            // zBox
+            // 
+            this.zBox.Location = new System.Drawing.Point(165, 12);
+            this.zBox.Name = "zBox";
+            this.zBox.Size = new System.Drawing.Size(32, 22);
+            this.zBox.TabIndex = 10;
+            // 
+            // randomBtn
+            // 
+            this.randomBtn.Location = new System.Drawing.Point(737, 67);
+            this.randomBtn.Name = "randomBtn";
+            this.randomBtn.Size = new System.Drawing.Size(75, 49);
+            this.randomBtn.TabIndex = 12;
+            this.randomBtn.Text = "Random C";
+            this.randomBtn.UseVisualStyleBackColor = true;
+            this.randomBtn.Visible = false;
+            this.randomBtn.Click += new System.EventHandler(this.randomBtn_Click);
+            // 
+            // defaultBtn
+            // 
+            this.defaultBtn.Location = new System.Drawing.Point(737, 122);
+            this.defaultBtn.Name = "defaultBtn";
+            this.defaultBtn.Size = new System.Drawing.Size(75, 49);
+            this.defaultBtn.TabIndex = 13;
+            this.defaultBtn.Text = "Default  C";
+            this.defaultBtn.UseVisualStyleBackColor = true;
+            this.defaultBtn.Visible = false;
+            this.defaultBtn.Click += new System.EventHandler(this.defaultBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 548);
+            this.Controls.Add(this.defaultBtn);
+            this.Controls.Add(this.randomBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.zBox);
+            this.Controls.Add(this.juliaSwitch);
+            this.Controls.Add(this.mandelSwitch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ImBox);
+            this.Controls.Add(this.ReBox);
+            this.Controls.Add(this.render);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button render;
+        private System.Windows.Forms.TextBox ReBox;
+        private System.Windows.Forms.TextBox ImBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton mandelSwitch;
+        private System.Windows.Forms.RadioButton juliaSwitch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox zBox;
+        private System.Windows.Forms.Button randomBtn;
+        private System.Windows.Forms.Button defaultBtn;
+    }
+}
+
---
+++ Fractals/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractals
+{
+    public partial class Form1 : Form
+    {
+        int n=100; // number of iterations
+        int zPow = 2; // Power of z function;
+        double c_re = -0.511;
+        double c_im = 0.541;
+        bool setToDefault = false;
+        bool generateRandom = false;
+        bool renderMandelbrot = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            zBox.Text = 2.ToString();
+        }
+        private double ParseToDouble(string s)
+        {
+            var sourceValue = s;
+            double doubleValue;
+            if (double.TryParse(sourceValue, out doubleValue))
+            {
+                return doubleValue;
+            }
+            else
+            {
+
+                return -100;
+            }
+        }
+        private void render_Click(object sender, EventArgs e)
+        {
+            if (nBox.Text == string.Empty || zBox.Text==string.Empty) return;
+            n = Int32.Parse(nBox.Text);
+            zPow = Int32.Parse(zBox.Text);
+            if (renderMandelbrot)
+                DrawMandelbrot(n,zPow);
+            else DrawJulia(n,zPow);
+        }
+        private void DrawMandelbrot(int n, int zPow = 2)
+        {
+            
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    Complex z = new Complex(0, 0);
+                    double re = (double)(x - (pictureBox1.Width / 2)) / (double)(pictureBox1.Width / 4);
+                    double im = (double)(y - (pictureBox1.Height / 2)) / (double)(pictureBox1.Height / 4);
+                    Complex c = new Complex(re, im);
+                    int iteration = 0;
+
+                    while (iteration <= n && z.GetModulus() < 2)
+                    {
+                        iteration++;
+                        Complex zValue = z;
+                        for (int i = 0; i < zPow - 1; i++)
+                        {
+                            z *= zValue;
+                        }
+                        z += c;
+
+                    }
+                    //Color edgeColor = (iteration <= n / (4) ? Color.Black : Color.Red);
+            
+                    bm.SetPixel(x, y, iteration < n ? Color.FromArgb(iteration%128*2,iteration%32*7,iteration%16*14) : Color.Black);
+                }
+            }
+            pictureBox1.Image = bm;
+        }
+
+
+        private void DrawJulia(int n, int zPow)
+        {
+           
+            Bitmap bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Complex c = new Complex();
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    double re = (double)(x - (pictureBox1.Width / 2)) / (double)(pictureBox1.Width / 4);
+                    double im = (double)(y - (pictureBox1.Height / 2)) / (double)(pictureBox1.Height / 4);
+
+                    if (setToDefault)
+                        c = new Complex(c_re, c_im);
+                    else if (generateRandom)
+                    {
+                        Random rnd = new Random();
+                        double a = rnd.NextDouble() * rnd.Next(-1, 1);
+                        if (a == 0)
+                            a=  rnd.NextDouble() * rnd.Next(-1, 1);
+                        double b = rnd.NextDouble() * rnd.Next(-1, 1);
+                        if (b == 0)
+                            a = rnd.NextDouble() * rnd.Next(-1, 1);
+                        c = new Complex(a,b);
+                        UpdateView(c);
+                        generateRandom = false;
+                        return;
+                     }
+                    else
+                    {
+                        double a = ParseToDouble(ReBox.Text);
+                        double b = ParseToDouble(ImBox.Text);
+
+                        if (a == -100 || b == -100)
+                        {
+                            MessageBox.Show("Invalid double input. Try ',' instead of '.'\n");
+                            return;
+                        }
+                        else
+                            c = new Complex(a, b);
+                    }
+                  
+                    Complex z = new Complex(re, im);
+                    int iteration = 0;
+
+                    while (iteration <= n && z.GetModulus() < 2)
+                    {
+                        iteration++;
+                        Complex zValue = z;
+                        for (int i = 0; i < zPow - 1; i++)
+                        {
+                            z *= zValue;
+                        }
+                        z += c;
+
+                    }
+                    bm.SetPixel(x, y, iteration < n ? Color.FromArgb(iteration % 128 * 2, iteration % 32 * 7, iteration % 16 * 14) : Color.Black);
+
+                }
+            }
+            UpdateView(c);
+            setToDefault = false;
+            pictureBox1.Image = bm;
+        }
+
+        private void juliaSwitch_MouseDown(object sender, MouseEventArgs e)
+        {
+            renderMandelbrot = false;
+            ReBox.ReadOnly = false;
+            ImBox.ReadOnly = false;
+            defaultBtn.Visible = true;
+            randomBtn.Visible = true;
+        }
+
+        private void mandelSwitch_MouseDown(object sender, MouseEventArgs e)
+        {
+            renderMandelbrot = true;
+            ReBox.ReadOnly = true;
+            ImBox.ReadOnly = true;
+            ReBox.Text = string.Empty;
+            ImBox.Text = string.Empty;
+            defaultBtn.Visible = false;
+            randomBtn.Visible = false;
+        }
+
+        private void defaultBtn_Click(object sender, EventArgs e)
+        {
+            setToDefault = true;
+            zPow = 2;
+            DrawJulia(n, zPow);
+        }
+        private void UpdateView(Complex c)
+        {
+            zBox.Text = zPow.ToString();
+            ReBox.Text = c.Re.ToString();
+            ImBox.Text = c.Im.ToString();
+        }
+
+        private void randomBtn_Click(object sender, EventArgs e)
+        {
+            generateRandom = true;
+            DrawJulia(n, zPow);
+        }
+    }
+}
